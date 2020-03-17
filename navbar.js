6fa29f2c56b1944993ade0c1ea88c81b05e7ba61
unknown <r.rzeszutek@interia.eu>
2020-03-17 17:09:20 +0100

    navigaion added

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav><a href="index.html">Home</a>' + '\t' +
+    '<a href="quiz.html">Quiz</a>' + '\t' +
+    '<a href="contact.html">Kontakt</a> </nav>';
+
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
