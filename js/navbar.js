12fa32c95a042ecf8d93e467956795414ed8635d
unknown <r.rzeszutek@interia.eu>
2020-03-24 14:15:10 +0100

    restructure

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 let nav = '<nav><a href="index.html">Home</a>' + '\t' +
     '<a href="quiz.html">Quiz</a>' + '\t' +
-    '<a href="contact.html">Kontakt</a> </nav>';
+    '<a href="contact.html">Kontakt</a></nav>';
 
 let navbarElem = document.querySelector(".navbar");
 
